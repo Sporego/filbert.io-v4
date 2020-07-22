887fb5f79960cc71bec0154540f29ebc2bd0524f
Sporego <Hello@Filbert.me>
2020-07-21 23:05:22 -0400

    navbar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -48,13 +48,16 @@
 	}
 </style>
 
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
-</nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	<a class="navbar-brand" href=".">filbert.io</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+	  <div class="navbar-nav">
+		<a class="nav-item nav-link active" aria-current="{segment === undefined ? 'page' : undefined}" href=".">home </a>
+		<a class="nav-item nav-link active" aria-current="{segment === 'projects' ? 'page' : undefined}" href="projects">projects</a>
+		<a class="nav-item nav-link active" rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a>
+	  </div>
+	</div>
+  </nav>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+	<title>projects</title>
+</svelte:head>
+
+<div class="container">
+
+	<h1>projects <span class="badge badge-secondary">New</span></h1>
+
+	<p>This is the 'about' page. There's not much here.</p>
+
+</div>
